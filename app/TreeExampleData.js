46b8802d614164d1b4b3e95165213ca82ebfd6ca
Tobias Wildgruber <tobias.wildgruber71@gmail.com>
2015-10-29 09:06:06 +0100

    ...

--- app/TreeExampleData.js
+++ app/TreeExampleData.js
@@ -1,6 +1,6 @@
 import n from './mkNode';
 
 export default [n('North America', {}, [n('USA', {
-            population: 3, someNameLikeProp: 'USA'
-        }, [n('South Carolina'), n('Texas'), n('Oregon'), n('South Dakota')]), n('Canada'), n('Mexico')], true),
+    population: 3, someNameLikeProp: 'USA'
+}, [n('South Carolina'), n('Texas'), n('Oregon'), n('South Dakota')]), n('Canada'), n('Mexico')], true),
     n('Europe', {}, [n('Norway'), n('Sweden'), n('France'), n('Germany')], true)];
